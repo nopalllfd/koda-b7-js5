88d4e4e09ea674ccd1ca534718832848feb69e03
Muhammad Naufal Fadhilah <117992655+nopalllfd@users.noreply.github.com>
2026-02-26 11:58:55 +0700

    Refactor updatePassword to use users array

--- minitask-5.js
+++ minitask-5.js
@@ -1,20 +1,42 @@
-let updatePassword = (newPassword) => {
-  let account = {
+let users = [
+  {
     id: 1,
     username: 'nopal',
     password: 'nopal',
-  };
+  },
+  {
+    id: 2,
+    username: 'nopal2',
+    password: 'nopal2',
+  },
+  {
+    id: 3,
+    username: 'nopal3',
+    password: 'nopal3',
+  },
+];
+let updatePassword = (userId, newPassword) => {
   let newAccount = {};
   if (newPassword.length < 5) {
     return console.log('Password minimum 5 karakter');
   } else {
-    newAccount = {
-      ...account,
-      password: newPassword,
-    };
+    let i = 0;
+    const account = users.find((u) => u.id == userId);
+    if (!account) {
+      return console.log('User tidak ditemukan');
+    }
+
+    if (account.id != undefined) {
+      newAccount = {
+        ...account,
+        password: newPassword,
+      };
+    } else {
+      return console.log('User tidak ditemukan');
+    }
   }
   const { username, password, id } = newAccount;
-  console.log(`Password updated : id ${id}, username : ${username}, Password  ${password}`);
+  console.log(`Password updated : id ${id}, username : ${username}, Password : ${password}`);
 };
 
-updatePassword('nopalkkkl');
+updatePassword(2, 'nopalkkkl');
